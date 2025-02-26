bb7488b120fd9a531b40a26425d2d43b8628af24
Tyler Vazquez <164375396+TylerxVzqz@users.noreply.github.com>
2025-02-26 18:00:36 -0500

    Create post.mjs

---
+++ post.mjs
@@ -0,0 +1,25 @@
+export default class Post
+{export default class Post
+{
+	constructor (title, content)
+	{
+		this.title = title;
+		this.content = content;
+	}
+
+	publish ()
+	{
+		console.log(`Publishing post: ${this.title}`);
+	}
+}
+	constructor (title, content)
+	{
+		this.title = title;
+		this.content = content;
+	}
+
+	publish ()
+	{
+		console.log(`Publishing post: ${this.title}`);
+	}
+}
